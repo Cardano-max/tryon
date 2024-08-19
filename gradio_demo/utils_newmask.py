4c7d529e9847dfb072fe85fc42bb7be595806a35
ateeb <mateeb.ce41ceme@ce.ceme.edu.pk>
2024-08-19 10:20:43 +0500

    updated utils_newmask-4

--- gradio_demo/utils_newmask.py
+++ gradio_demo/utils_newmask.py
@@ -87,7 +87,7 @@ class Masking:
         mask_pil = Image.fromarray((mask * 255).astype(np.uint8))
         mask_pil = mask_pil.resize(img.size, Image.LANCZOS)
         
-        return np.array(mask_pil)
+        return mask_pil
 
     def create_precise_hand_mask(self, image):
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
