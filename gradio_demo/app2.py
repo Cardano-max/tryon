ddcb7f62b422a81c65c10a52b438842dd6caafe9
ateeb <mateeb.ce41ceme@ce.ceme.edu.pk>
2024-08-27 17:03:04 +0500

    debugging

--- gradio_demo/app2.py
+++ gradio_demo/app2.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append('/Users/ateeb.taseer/tryon2/arbi-tryon')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import gradio as gr
 import cv2
