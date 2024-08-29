72cf3ef74635ef415aab343f02e21da25a1fc4c7
ateeb <mateeb.ce41ceme@ce.ceme.edu.pk>
2024-08-29 13:40:28 +0500

    adpaters

--- preprocess/masking/__init__.py
+++ preprocess/masking/__init__.py
@@ -1,1 +1,11 @@
-from .masking import Masking
+from .mask_generator import MaskGenerator
+from .factory import MaskFactory
+
+class Masking:
+    def __init__(self):
+        self.factory = MaskFactory()
+        self.mask_generator = MaskGenerator()
+
+    def get_mask(self, image, category='dresses', model_type='hd'):
+        adapter = self.factory.get_mask_adapter(model_type)
+        return adapter.generate_mask(image, category)
